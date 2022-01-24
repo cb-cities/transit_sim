0865149aa95bc7d6a7e04c970762867a8e4cb6e7
Bingyu Zhao <bingyu@Bingyus-MacBook.local>
2022-01-23 19:31:08 -0800

    add transfer

--- model/transit_sim_model.py
+++ model/transit_sim_model.py
@@ -27,8 +27,24 @@ class Network():
 
         ### create geometry dictionary for visualization
         self.station_locations = {getattr(row, 'route_stop_id'): getattr(row, 'geometry') for row in self.all_nodes.itertuples()}
-        self.link_sections = {'{}-{}'.format(getattr(row, 'route_stop_id'), getattr(row, 'next_route_stop_id')):
-            getattr(row, 'geometry').interpolate(0.2, 0.3) for row in self.all_links.itertuples()}
+        #self.link_sections = {'{}-{}'.format(getattr(row, 'route_stop_id'), getattr(row, 'next_route_stop_id')):
+        #    getattr(row, 'geometry').interpolate(0.2, 0.3) for row in self.all_links.itertuples()}
+        self.station_cx = dict()
+        self.station_cy = dict()
+        self.link_cx = dict()
+        self.link_cy = dict()
+        self.link_ux = dict()
+        self.link_uy = dict()
+        for row in self.all_nodes.itertuples():
+            self.station_cx[getattr(row, 'route_stop_id')] = getattr(row, 'geometry').x
+            self.station_cy[getattr(row, 'route_stop_id')] = getattr(row, 'geometry').y
+        for row in self.all_links.itertuples():
+            link_nm = '{}-{}'.format(getattr(row, 'route_stop_id'), getattr(row, 'next_route_stop_id'))
+            link_geom = getattr(row, 'geometry')
+            self.link_cx[link_nm] = link_geom.interpolate(0.2, 0.3).x
+            self.link_cy[link_nm] = link_geom.interpolate(0.2, 0.3).y
+            self.link_ux[link_nm] = link_geom.coords[-1][0] - link_geom.coords[0][0]
+            self.link_uy[link_nm] = link_geom.coords[-1][1] - link_geom.coords[0][1]
 
 class Trains():
     def __init__(self):
@@ -45,6 +61,11 @@ class Trains():
                                   getattr(row, 'departure_time'), getattr(row, 'next_arrival_time'), 'on_link',
                                   '{}-{}'.format(getattr(row, 'route_stop_id'), 
                                                  getattr(row, 'next_route_stop_id'))))
+        ### add final destination of the trip
+        for row in schedule_table.groupby('trip_id').tail(1).itertuples():
+            schedule_list.append((getattr(row, 'trip_id'),
+                                  getattr(row, 'next_arrival_time'), getattr(row, 'next_arrival_time')+30, 'stop', 
+                                  getattr(row, 'next_route_stop_id')))
         self.schedule_df = pd.DataFrame(schedule_list, columns=['trip_id', 'time', 'next_time', 'status', 'location'])
     
     def add_network(self, schedule_table):
@@ -65,7 +86,7 @@ class Trains():
         ### station nodes
         virtual_nodes = all_nodes.groupby('stop_id').agg({'stop_lon': np.mean, 'stop_lat': np.mean}).reset_index(drop=False)
         virtual_nodes['stop_lon'] *= 0.999999
-        virtual_nodes['route_stop_id'] = virtual_nodes['stop_id'].apply(lambda x: 'all_{}'.format(x))
+        virtual_nodes['route_stop_id'] = virtual_nodes['stop_id'].apply(lambda x: 'all-{}'.format(x))
         virtual_nodes['type'] = 'station'
         all_nodes = pd.concat([all_nodes, virtual_nodes[all_nodes.columns]])
         all_nodes['node_id'] = np.arange(all_nodes.shape[0])
@@ -100,7 +121,7 @@ class Trains():
         all_links = gpd.GeoDataFrame(all_links, crs='epsg:4326', geometry=all_links['geometry'].map(loads))
         return all_nodes, all_links
     
-    def schedule_and_network_from_gtfs(self, stop_times_file, trips_file, stops_file):
+    def schedule_and_network_from_gtfs(self, stop_times_file, trips_file, stops_file, service_id):
         ### read GTFS files
         stop_times_table = pd.read_csv(stop_times_file)
         trips_table = pd.read_csv(trips_file)
@@ -109,9 +130,10 @@ class Trains():
         ### merge the tables
         schedule_table = stop_times_table[['trip_id', 'arrival_time', 'departure_time', 'stop_id']]
         ### assign a route code to individual trains
+        trips_table = trips_table[trips_table['service_id']==service_id]
         schedule_table = pd.merge(schedule_table, trips_table[['trip_id', 'route_id']],
-                                   how='left', on='trip_id')
-        ### assign a route code to individual stops
+                                   how='right', on='trip_id')
+        ### assign a route-stop code to individual stops
         schedule_table['route_stop_id'] = schedule_table.apply(lambda x:
                                                                '{}-{}'.format(x['route_id'], x['stop_id']), axis=1)
         ### assign locations to individual stations
@@ -166,11 +188,18 @@ class Trains():
         
     def get_all_train_positions(self, network):
         ### get train position
-        train_positions = self.schedule_df[self.schedule_df['current_location']=='current'].copy()
-        train_positions['geometry'] = np.where(train_positions['status']=='stop',
-                                              train_positions['location'].map(network.station_locations),
-                                              train_positions['location'].map(network.link_sections))
-        train_positions = gpd.GeoDataFrame(train_positions, crs='epsg:4326', geometry=train_positions['geometry'])
+        train_positions = self.schedule_df.loc[self.schedule_df['current_location']=='current', 
+                                           ['trip_id', 'status', 'location']].copy()
+        train_positions['cx'] = np.where(train_positions['status']=='stop',
+                                              train_positions['location'].map(network.station_cx),
+                                              train_positions['location'].map(network.link_cx))
+        train_positions['cy'] = np.where(train_positions['status']=='stop',
+                                              train_positions['location'].map(network.station_cy),
+                                              train_positions['location'].map(network.link_cy))
+        train_positions['ux'] = np.where(train_positions['status']=='stop',
+                                              0, train_positions['location'].map(network.link_ux))
+        train_positions['uy'] = np.where(train_positions['status']=='stop',
+                                              0, train_positions['location'].map(network.link_uy))
         return train_positions
         
 class Travelers():
@@ -181,21 +210,26 @@ class Travelers():
        
     def random_od(self, all_nodes=None, num_travelers=1):
         
-        #traveler_origins = np.random.choice(all_nodes['node_id'], num_travelers)
-        #traveler_destins = np.random.choice(all_nodes['node_id'], num_travelers)
-        traveler_origins = [9]
-        traveler_destins = [22]
+        od_nodes = all_nodes['route_stop_id'].str.split('-').str[0]=='all'
+        traveler_origins = np.random.choice(all_nodes.loc[od_nodes, 'node_id'], num_travelers)
+        traveler_destins = np.random.choice(all_nodes.loc[od_nodes, 'node_id'], num_travelers)
+        #traveler_origins = [241]
+        #traveler_destins = [267]
         self.travelers_df = pd.DataFrame({
             'origin_nid': traveler_origins, 'destin_nid': traveler_destins})
         self.travelers_df = self.travelers_df[self.travelers_df['origin_nid'] != self.travelers_df['destin_nid']].copy()
         self.travelers_df['traveler_id'] = np.arange(self.travelers_df.shape[0])
-        self.travelers_df['departure_time'] = np.random.randint(25980, 26980, self.travelers_df.shape[0])
+        self.travelers_df['departure_time'] = np.random.randint(26664, 30000, self.travelers_df.shape[0])
+        #self.travelers_df['departure_time'] = 26664-120
     
     def set_initial_status(self, station_id_nm_dict):
         ### initialize traveler_df
         self.travelers_df['traveler_status'] = 'pretrip'
+        self.travelers_df['update_time'] = 0
         self.travelers_df['association'] = None 
         self.travelers_df['next_station'] = None
+        self.travelers_df['origin_station'] = self.travelers_df['origin_nid'].map(station_id_nm_dict)
+        self.travelers_df['destination_station'] = self.travelers_df['destin_nid'].map(station_id_nm_dict)
     
     def find_routes(self, network_g, station_id_nm_dict):
         ### find paths using Dijkstra's algorithm
@@ -218,13 +252,11 @@ class Travelers():
                 sp.clear()
 
                 key_stops = []
-                current_route = None ### current line number/name
-                for stop in traveler_path[:-1]: ### always append the last element, so no need to iterate through
-                    new_route = stop.split('-')[0]
-                    if new_route != current_route:
-                        key_stops.append(stop)
-                        current_route = new_route
-                key_stops.append(traveler_path[-1])
+                for stop, next_stop in zip(traveler_path, traveler_path[1:]):
+                    route = stop.split('-')[0]
+                    next_route = next_stop.split('-')[0]
+                    if route != next_route:
+                        key_stops += [stop, next_stop]
 
             self.travelers_paths[getattr(traveler, 'traveler_id')] = traveler_path
             self.travelers_key_stops[getattr(traveler, 'traveler_id')] = {
@@ -249,32 +281,60 @@ class Travelers():
         ### lookup dictionary 2: from trip_id to platform
         stop_trip_ids_dict = {getattr(train, 'trip_id'): 
                                getattr(train, 'location') for train in stop_trains.itertuples()}
+        #print({k:v for k,v in stop_train_locations_dict.items() if v==966030})
+        #print({k:v for k,v in stop_trip_ids_dict.items() if k==966030})
         
         ### load departure travelers
-        ### (1) change status from "pretrip" to "platform"
+        ### (1) change status from "pretrip" to "walking"
         ### (2) change association from "None" to "origin_nid"
         ### (3) change next_stop from "None" to next key stop
         departure_travelers = (
-            self.travelers_df['departure_time']<t) & (
+            self.travelers_df['departure_time']<=t) & (
             self.travelers_df['traveler_status']=='pretrip')
-        self.travelers_df.loc[departure_travelers, 'traveler_status'] = 'platform'
+        self.travelers_df.loc[departure_travelers, 'traveler_status'] = 'walking'
+        self.travelers_df.loc[departure_travelers, 'update_time'] = t
         self.travelers_df.loc[departure_travelers, 'association'] = self.travelers_df.loc[
-            departure_travelers, 'origin_nid'].map(network.station_id_nm_dict)
+            departure_travelers, 'origin_station']
         self.travelers_df.loc[departure_travelers, 'next_station'] = self.travelers_df.loc[
-            departure_travelers].apply(lambda x: self.find_next_station(x) , axis=1)
+            departure_travelers].apply(lambda x: self.find_next_station(x), axis=1)
+        
+        ### transfer
+        ### conditions: status is "walking" and transfer time is 2 minutes
+        walking_travelers = (
+            self.travelers_df['traveler_status']=='walking') & (
+            self.travelers_df['update_time']<=t-120)
+        self.travelers_df.loc[walking_travelers, 'traveler_status'] = 'platform'
+        self.travelers_df.loc[walking_travelers, 'update_time'] = t
+        self.travelers_df.loc[walking_travelers, 'association'] = self.travelers_df.loc[walking_travelers, 'next_station']
+        self.travelers_df.loc[walking_travelers, 'next_station'] = self.travelers_df.loc[
+            walking_travelers].apply(lambda x: self.find_next_station(x), axis=1)
+        #print(t, ' arrive at platform ', self.travelers_df.loc[walking_travelers, 'traveler_id'])
+        
+        ### arrival
+        arrival_travelers = (
+            self.travelers_df['traveler_status']=='platform') & (
+            self.travelers_df['association'] == self.travelers_df['destination_station'])
+        self.travelers_df.loc[arrival_travelers, 'traveler_status'] = 'arrival'
+        self.travelers_df.loc[arrival_travelers, 'update_time'] = t
+        self.travelers_df.loc[arrival_travelers, 'association'] = None
+        self.travelers_df.loc[arrival_travelers, 'next_station'] = None
         
         ### aboard: travelers ready to aboard
+        ### check if next stop is covered
         ### conditions: status is "platform"
         board_travelers = self.travelers_df['traveler_status']=='platform'
-        ### (1) change association from platform to trip_id
+        board_travelers = board_travelers & self.travelers_df['association'].isin(stop_train_locations_dict.keys())
+        ### (1) change next stop according to key routes
+        self.travelers_df.loc[board_travelers, 'next_station'] = self.travelers_df.loc[
+            board_travelers].apply(lambda x: self.find_next_station(x), axis=1)
+        ### (2) change association from platform to trip_id
+        #print(self.travelers_df.loc[board_travelers])
         self.travelers_df.loc[board_travelers, 'association'] = self.travelers_df.loc[board_travelers, 
                                                                      'association'].replace(stop_train_locations_dict)
-        ### (2) change status from "platform" to "train"
+        ### (3) change status from "platform" to "train"
         self.travelers_df['traveler_status'] = np.where(
             board_travelers, 'train', self.travelers_df['traveler_status'])
-        ### (3) change next stop according to key routes
-        self.travelers_df.loc[board_travelers, 'next_station'] = self.travelers_df.loc[
-            board_travelers].apply(lambda x: self.find_next_station(x) , axis=1)
+        self.travelers_df.loc[board_travelers, 'update_time'] = t
 
         ### alight: travelers ready to get off the train
         ### conditions: (1) status is "train"
@@ -286,31 +346,22 @@ class Travelers():
         self.travelers_df.loc[alight_travelers, 'association'] = self.travelers_df.loc[alight_travelers, 
                                                                      'association'].replace(stop_trip_ids_dict)
         ### (2) change status from "train" to "platform"
-        self.travelers_df['traveler_status'] = np.where(alight_travelers, 'platform', self.travelers_df['traveler_status'])
+        self.travelers_df['traveler_status'] = np.where(alight_travelers, 'walking', self.travelers_df['traveler_status'])
+        self.travelers_df.loc[alight_travelers, 'update_time'] = t
         ### (3) change next stop according to key routes
         self.travelers_df.loc[alight_travelers, 'next_station'] = self.travelers_df.loc[
-            alight_travelers, 'next_station'].apply(lambda x: self.find_next_station(x) , axis=1)
+            alight_travelers].apply(lambda x: self.find_next_station(x), axis=1)
     
-    def get_all_traveler_positions(self, station_locations=None, train_positions=None):
+    def get_all_traveler_positions(self, train_positions):
         
         ### get traveler locations
         ### group by station or train location
         traveler_locations = self.travelers_df.groupby(
             ['traveler_status', 'association']).size().to_frame(
             name='num_travelers').reset_index(drop=False)
-        traveler_locations['association'] = traveler_locations['association'].astype(str)
-        traveler_locations['geometry'] = None
-        ### assign geometry to stations
-        traveler_locations['geometry'] = traveler_locations['geometry'].map(station_locations)
-        ### assign geometry to travelers on trains
-        train_positions['trip_id'] = train_positions['trip_id'].astype(str)
-        traveler_locations = pd.merge(
-            traveler_locations, train_positions[['trip_id', 'geometry']], 
-            how='left', left_on='association', right_on='trip_id', suffixes=['_platform', '_train'])
-        traveler_locations['geometry'] = np.where(pd.isnull(traveler_locations['geometry_platform']), 
-                                                    traveler_locations['geometry_train'],
-                                                    traveler_locations['geometry_platform'])
-        traveler_locations = traveler_locations[['association', 'num_travelers', 'geometry']]
-        traveler_locations = gpd.GeoDataFrame(traveler_locations, crs='epsg:4326', 
-                                                 geometry=traveler_locations['geometry'])
+        
+        travelers_on_trains = traveler_locations[traveler_locations['traveler_status']=='train']
+        travelers_on_trains['association'] = travelers_on_trains['association'].astype(int)
+        travelers_on_trains = travelers_on_trains.merge(train_positions[['trip_id', 'cx', 'cy']], 
+                                                        how='left', left_on='association', right_on='trip_id')
         return traveler_locations
